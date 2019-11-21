02da0eeb81285dc1f3e65012878a941965320f70
maksdk <havryshchenko@gmail.com>
2019-11-21 08:58:31 +0200

    logic constructions

--- racket/main.rkt
+++ racket/main.rkt
@@ -10,5 +10,16 @@
         
 (displayln (square-of-sum 2 2))
 
+(define (same-parity? x y) 
+   (or (and (odd? x) (odd? y)) 
+      (and (even? x) (even? y))))
 
-(define (same-parity? x y) (or (and (odd? x) (odd? y)) (and (even? x) (even? y))))
+(define (gt? x y) (> x y))
+(displayln (gt? 2 2)) ;false
+(displayln (gt? 4 2)) ;true
+
+(displayln (even? 3)) ;false
+(displayln (even? 4)) ;true
+
+(displayln (odd? 3)) ;true
+(displayln (odd? 4)) ;false
